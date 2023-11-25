2b8026e2d8a37dc786d09c5591e309e63a1d33fa
steveelucky <jsjggroups@gmail.com>
2023-11-25 11:56:13 -0500

    Initial commit

--- src/Student_Class.cpp
+++
@@ -1,4 +0,0 @@
-This text appears to be discussing various aspects of the human psychological profile and how it's influenced by different 
-factors. It emphasizes that our psychological makeup is shaped by elements such as personal space, decision-making tools
-, preferences, self-efficacy, initiatives, priorities, cognitive behavior, mimicry, gestures, and more. The text also delves into 
-the idea that our psychological profile plays a significant role in how we perceive and interact with the world.
